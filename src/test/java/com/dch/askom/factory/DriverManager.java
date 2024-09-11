b0bd0f26c47f6b0182261fe679d5243ae7a1fb87
chamaraf <developandtestanddeploy@gmail.com>
2024-09-11 20:32:13 +0100

    Driver manager completed

--- src/test/java/com/dch/askom/factory/DriverManager.java
+++ src/test/java/com/dch/askom/factory/DriverManager.java
@@ -1,4 +1,26 @@
 package com.dch.askom.factory;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 public class DriverManager {
+
+    public static WebDriver initializeDriver(String browser) {
+        WebDriver driver;
+        switch (browser) {
+            case "chrome": driver = new ChromeDriver();
+            break;
+            case "firefox": driver = new FirefoxDriver();
+            break;
+            case "ie": driver = new InternetExplorerDriver();
+            break;
+            default: throw new RuntimeException("Invalid browser: " + browser);
+        }
+
+        driver.manage().window().maximize();
+        return driver;
+    }
+
 }
